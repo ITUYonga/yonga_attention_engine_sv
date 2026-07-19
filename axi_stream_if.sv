b236f2693c0d9008dea9ecec88d1cad83884e3fd
mta1114 <aydemirm22@itu.edu.tr>
2026-07-19 15:39:04 +0300

    Update axi_stream_if.sv

--- axi_stream_if.sv
+++ axi_stream_if.sv
@@ -17,30 +17,35 @@ module axi_stream_if #(
     output logic        internal_rx_last //FSM son data geldi sinyali ver
 
 
-    output logic [DATA_WIDTH_bf16-1:0] m_axis_tdata,   // output bf16 data
+    output logic [DATA_WIDTH_bf16-1:0] m_axis_tdata,   // output data
     output logic        m_axis_tvalid,  // PC data var sinyali ver
     input  logic        m_axis_tready,  // PC data alabilirim sinyali al
     output logic        m_axis_tlast,   // PC Data bitti sinayi ver
 
 
-    input  logic [DATA_WIDTH_bf16-1:0] internal_tx_data,  // FSM'den gelen data
+    input  logic [DATA_WIDTH_bf16-1:0] internal_tx_data,  // FIFO'dan gelen data
     input  logic        internal_tx_valid, // FSM data hazir sinyal
-    output logic        internal_tx_ready, // FSM data alabilirim sinyali
+    output logic        internal_tx_ready, // FIFO data alabilirim sinyali
     input  logic        internal_tx_last   // FSM data bitti sinyal al
 );
-    //bf16
+    //f32-bf16
     bf16_comb bf16(
-        fp_32in(s_axis_tdata),
-        sel_i(select_bf16),
-        bf16_out(dbuf_rx_data)
+        .fp_32in(s_axis_tdata),
+        .sel_i(select_bf16),
+        .bf16_out(dbuf_rx_data)
     );
     //RX
     assign s_axis_tready = ~dbuf_rx_full;
     assign dbuf_rx_we    = s_axis_tvalid && s_axis_tready;
     assign internal_rx_last = s_axis_tlast;
     //TX
-    assign m_axis_tdata  = internal_tx_data;
     assign m_axis_tvalid = internal_tx_valid;
     assign m_axis_tlast  = internal_tx_last;
     assign internal_tx_ready = m_axis_tready;
+    //bf16-f32
+    bf16_convert fp32(
+        .bf16_in(internal_tx_data),
+        .fp32_out(m_axis_tdata)
+    )
+
 endmodule
